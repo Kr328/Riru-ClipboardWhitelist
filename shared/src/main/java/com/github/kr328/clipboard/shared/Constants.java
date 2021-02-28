e2bd21e96c9f5ca352150cc353a49a53b4224e60
kr328 <kr328app@outlook.com>
2021-02-28 19:37:52 +0800

    repick server transact code

--- shared/src/main/java/com/github/kr328/clipboard/shared/Constants.java
+++ shared/src/main/java/com/github/kr328/clipboard/shared/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String TAG = "ClipboardWhitelist";
     public static final String APP_PACKAGE_NAME = "com.github.kr328.clipboard";
 
-    public static final int TRANSACT_CODE_GET_SERVICE = 114514;
+    public static final int TRANSACT_CODE_GET_SERVICE = '_' << 24 | 'C' << 16 | 'B' << 8 | 'W';
 
-    public static final int SERVICE_VERSION = 1;
+    public static final int SERVICE_VERSION = 2;
 }
