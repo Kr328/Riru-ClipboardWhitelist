9146582ee9778b50c090b7c823add6196f4bf927
kr328 <kr328app@outlook.com>
2021-05-15 16:34:55 +0800

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ allprojects {
         val buildMinVersion: Int by extra(29)
         val buildTargetVersion: Int by extra(30)
 
-        val buildVersionCode: Int by extra(9)
-        val buildVersionName: String by extra("v9")
+        val buildVersionCode: Int by extra(10)
+        val buildVersionName: String by extra("v10")
 
         val buildNdkVersion: String by extra("23.0.7123448")
     }
