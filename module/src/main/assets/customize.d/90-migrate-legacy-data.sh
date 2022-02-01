05f1d5fa9f86e54b58b6be4c081cb8fe179b28a5
Kr328 <kr328app@outlook.com>
2022-02-01 20:55:55 +0800

    Chore: fix migrate message print

--- module/src/main/assets/customize.d/90-migrate-legacy-data.sh
+++ module/src/main/assets/customize.d/90-migrate-legacy-data.sh
@@ -1,8 +1,8 @@
 # Migrate legacy data to ZygoteLoader data directory
 
-ui_print "- Migrate legacy data directory"
-
 if [ -f "/data/misc/clipboard/whitelist.list" ]; then
+  ui_print "- Migrate legacy data directory"
+
   mkdir -p "$MODULE_DATA_PATH"
   cp -f "/data/misc/clipboard/whitelist.list" "$MODULE_DATA_PATH/whitelist.list"
   rm -rf "/data/misc/clipboard"
